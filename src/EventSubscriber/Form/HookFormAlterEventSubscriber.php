6d0a04ea8ee0e56865f529cabafa1adcf0ded05b
Ambient.Impact <i@ambientimpact.com>
2019-06-23 09:35:38 -0400

    Moved the Form component to ambientimpact_ux.

---
+++ src/EventSubscriber/Form/HookFormAlterEventSubscriber.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Drupal\ambientimpact_ux\EventSubscriber\Form;
+
+use Drupal\hook_event_dispatcher\HookEventDispatcherInterface;
+use Drupal\hook_event_dispatcher\Event\Form\FormAlterEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+ * hook_form_alter() event subscriber class.
+ */
+class HookFormAlterEventSubscriber implements EventSubscriberInterface {
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    return [
+      HookEventDispatcherInterface::FORM_ALTER => 'formAlter',
+    ];
+  }
+
+  /**
+   * Alter all forms.
+   *
+   * This does the following:
+   *
+   * - Attaches the form component library.
+   *
+   * @param \Drupal\hook_event_dispatcher\Event\Form\FormAlterEvent $event
+   *   The event object.
+   */
+  public function formAlter(FormAlterEvent $event) {
+    $form = &$event->getForm();
+
+    $form['#attached']['library'][] = 'ambientimpact_ux/component.form';
+  }
+}
