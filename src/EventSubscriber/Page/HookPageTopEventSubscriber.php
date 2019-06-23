ed2a3f3f8b725fa968d242435c1010c041ad83f6
Ambient.Impact <i@ambientimpact.com>
2019-06-23 01:06:22 -0400

    Moved the ToTop component and related event subscribers to ambientimpact_ux.

---
+++ src/EventSubscriber/Page/HookPageTopEventSubscriber.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Drupal\ambientimpact_ux\EventSubscriber\Page;
+
+use Drupal\ambientimpact_core\ComponentPluginManager;
+use Drupal\hook_event_dispatcher\HookEventDispatcherInterface;
+use Drupal\hook_event_dispatcher\Event\Page\PageTopEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+ * hook_page_top() event subscriber class.
+ */
+class HookPageTopEventSubscriber implements EventSubscriberInterface {
+  /**
+   * The Ambient.Impact Component plugin manager service.
+   *
+   * @var \Drupal\ambientimpact_core\ComponentPluginManager
+   */
+  protected $componentManager;
+
+  /**
+   * Event subscriber constructor; saves dependencies.
+   *
+   * @param \Drupal\ambientimpact_core\ComponentPluginManager $componentManager
+   *   The Ambient.Impact Component plugin manager service.
+   */
+  public function __construct(
+    ComponentPluginManager $componentManager
+  ) {
+    $this->componentManager = $componentManager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    return [
+      HookEventDispatcherInterface::PAGE_TOP => 'pageTop',
+    ];
+  }
+
+  /**
+   * Add renderable arrays to the top of the page.
+   *
+   * This adds the following:
+   *
+   * - An anchor to the very top of the page for the 'to_top' component to
+   *   scroll to.
+   *
+   * @param \Drupal\hook_event_dispatcher\Event\Page\PageTopEvent $event
+   *   The event object.
+   */
+  public function pageTop(PageTopEvent $event) {
+    $build        = &$event->getBuild();
+    $toTopConfig  = $this->componentManager
+                      ->getComponentConfiguration('to_top');
+
+    $build['top_anchor'] = [
+      '#type'       => 'html_tag',
+      '#tag'        => 'a',
+      '#attributes' => [
+        'id'  => $toTopConfig['topAnchorID'],
+      ],
+      '#weight'     => -999,
+    ];
+  }
+}
---
+++ src/Plugin/AmbientImpact/Component/ToTop.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Drupal\ambientimpact_ux\Plugin\AmbientImpact\Component;
+
+use Drupal\ambientimpact_core\ComponentBase;
+use Drupal\Component\Utility\Html;
+
+/**
+ * To Top component.
+ *
+ * @Component(
+ *   id = "to_top",
+ *   title = @Translation("To Top"),
+ *   description = @Translation("Scroll to the top of the page via a button-style link that shows and hides based on scroll direction and distance to the top of the page.")
+ * )
+ */
+class ToTop extends ComponentBase {
+  /**
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration() {
+    return [
+      'topAnchorID' => Html::getUniqueId('top'),
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getJSSettings(): array {
+    $config = $this->configuration;
+
+    return [
+      'topAnchorID' => $config['topAnchorID'],
+    ];
+  }
+}
