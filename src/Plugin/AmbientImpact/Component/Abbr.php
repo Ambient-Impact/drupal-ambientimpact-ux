4f9993d0a4f5b37943360eea44ab2d079f6631b4
Ambient.Impact <i@ambientimpact.com>
2019-06-23 08:36:33 -0400

    Moved the Abbr component to ambientimpact_ux.

---
+++ src/Plugin/AmbientImpact/Component/Abbr.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Drupal\ambientimpact_ux\Plugin\AmbientImpact\Component;
+
+use Drupal\ambientimpact_core\ComponentBase;
+
+/**
+ * Abbreviation component.
+ *
+ * @Component(
+ *   id = "abbr",
+ *   title = @Translation("Abbreviation (HTML <abbr> element)"),
+ *   description = @Translation("Provides rich, mobile-accessible tooltips for <abbr> elements.")
+ * )
+ */
+class Abbr extends ComponentBase {
+}
