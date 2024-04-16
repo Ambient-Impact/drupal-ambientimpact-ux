8fb1a82e8d5a978346fba0ec0bc23eaee293eeef
Matei Stanca <i@ambientimpact.com>
2024-04-16 02:46:43 -0400

    Issue #1: Expanded Tooltip demo to showcase inline positioning and more.

--- src/Plugin/AmbientImpact/Component/Tooltip.php
+++ src/Plugin/AmbientImpact/Component/Tooltip.php
@@ -32,14 +32,26 @@ class Tooltip extends ComponentBase {
     /** @var \Drupal\Core\Link */
     $abbrMdnLink = new Link(
       $this->t('<code>&lt;abbr&gt;</code> element'), Url::fromUri(
-        'https://developer.mozilla.org/en-US/docs/Web/HTML/Element/abbr',
+        'https://developer.mozilla.org/en-US/docs/Web/HTML/Element/abbr', [
+          'attributes' => [
+            'title' => $this->t(
+              'The &lt;abbr&gt; HTML element represents an abbreviation or acronym',
+            ),
+          ],
+        ],
       ),
     );
 
     /** @var \Drupal\Core\Link */
     $titleAttrMdnLink = new Link(
       $this->t('<code>title</code> attribute'), Url::fromUri(
-        'https://developer.mozilla.org/en-US/docs/Web/HTML/Global_attributes/title',
+        'https://developer.mozilla.org/en-US/docs/Web/HTML/Global_attributes/title', [
+          'attributes' => [
+            'title' => $this->t(
+              'The title global attribute contains text representing advisory information related to the element it belongs to',
+            ),
+          ],
+        ],
       ),
     );
 
@@ -49,8 +61,58 @@ class Tooltip extends ComponentBase {
         'https://developer.mozilla.org', [
           'attributes' => [
             'target'  => '_blank',
-            'title'   => $this->t('You won\'t see this on mobile until you return to this page.'),
-          ]
+            'title'   => $this->t(
+              'You won\'t see this on mobile until you return to this page',
+            ),
+          ],
+        ],
+      ),
+    );
+
+    /** @var \Drupal\Core\Link */
+    $longLink1 = new Link($this->t(
+      'to choose the position of the tooltip based on the closest text fragment',
+      ), Url::fromUri(
+        'https://atomiks.github.io/tippyjs/v6/all-props/#inlinepositioning', [
+          'attributes' => [
+            'title'   => $this->t('Hello!'),
+          ],
+        ],
+      ),
+    );
+
+    /** @var \Drupal\Core\Link */
+    $longLink2 = new Link($this->t(
+      'via the <code>inlinePositioning</code> property',
+      ), Url::fromUri(
+        'https://atomiks.github.io/tippyjs/v6/all-props/#inlinepositioning', [
+          'attributes' => [
+            'title' => $this->t('Hello again!'),
+          ],
+        ],
+      ),
+    );
+
+    /** @var \Drupal\Core\Link */
+    $longLink3 = new Link($this->t(
+      'on these links at the end or start of a line where they wrap',
+      ), Url::fromUri(
+        'https://developer.mozilla.org/en-US/docs/Web/API/Element/getClientRects', [
+          'attributes' => [
+            'title' => $this->t('Hellooooo!'),
+          ],
+        ],
+      ),
+    );
+
+    /** @var \Drupal\Core\Link */
+    $longLink4 = new Link($this->t(
+      'resize your browser window if you\'re able to, or rotate your mobile device',
+      ), Url::fromUri(
+        'https://developer.mozilla.org/en-US/docs/Learn/CSS/CSS_layout/Responsive_Design', [
+          'attributes' => [
+            'title' => $this->t('Henlo!'),
+          ],
         ],
       ),
     );
@@ -86,13 +148,15 @@ class Tooltip extends ComponentBase {
         'abbr_demo' => [
           '#type'     => 'html_tag',
           '#tag'      => 'blockquote',
-          '#value'    => $this->t('The fundamental languages of the web are <abbr title="HyperText Markup Language">HTML</abbr>, <abbr title="Cascading Style Sheets">CSS</abbr>, and JavaScript. These are delivered to browsers using <abbr title="HyperText Transfer Protocol">HTTP</abbr> or <abbr title="HyperText Transfer Protocol Secure">HTTPS</abbr>, the latter of which is preferable because it encrypts data via <abbr title="Transport Layer Security">TLS</abbr> or the now deprecated <abbr title="Secure Sockets Layer">SSL</abbr>.'),
+          '#value'    => $this->t(
+            'The fundamental languages of the web are <abbr title="HyperText Markup Language">HTML</abbr>, <abbr title="Cascading Style Sheets">CSS</abbr>, and JavaScript. These are delivered to browsers using <abbr title="HyperText Transfer Protocol">HTTP</abbr> or <abbr title="HyperText Transfer Protocol Secure">HTTPS</abbr>, the latter of which is preferable because it encrypts data via <abbr title="Transport Layer Security">TLS</abbr> or the now deprecated <abbr title="Secure Sockets Layer">SSL</abbr>.',
+          ),
         ],
         'button_link_description' => [
           '#type'     => 'html_tag',
           '#tag'      => 'p',
           '#value'    => $this->t(
-            'Buttons and links are another common use for tooltips, but beware that this is not recommended unless special care is taken since touch screens don\'t usually provide a way for a user to hover over an element before activating it, and so would result in bad <abbr title="user experience">UX</abbr>. For an example, tap the following link on a mobile device (it opens in a new tab) and then hit your back button to return to this page: @mdnLink',
+            'Buttons and links are another common use for tooltips, but beware that this is not recommended unless special care is taken since touch screens don\'t usually provide a way for a user to hover over an element before activating it, and so would result in pretty bad <abbr title="user experience">UX</abbr>. For an example, tap the following link on a mobile device (it opens in a new tab) and then hit your back button to return to this page: @mdnLink',
             [
               '@mdnLink' => $mdnLink->toString(),
             ],
@@ -118,6 +182,19 @@ class Tooltip extends ComponentBase {
               ),
             ],
           ],
+        ],
+        'long_links' => [
+          '#type'     => 'html_tag',
+          '#tag'      => 'p',
+          '#value'    => $this->t(
+            'Another neat trick Tippy.js can do is @longLink1 when text wraps over more than one line @longLink2, which we set to true by default. Try hovering over these or long pressing on a touch screen @longLink3 and watch the tooltip appear where it feels most intuitive rather than at the centre of the entire links\' bounding boxes. In the unlikely case none of these links wrap to a new line, @longLink4.',
+            [
+              '@longLink1' => $longLink1->toString(),
+              '@longLink2' => $longLink2->toString(),
+              '@longLink3' => $longLink3->toString(),
+              '@longLink4' => $longLink4->toString(),
+            ],
+          ),
         ],
       ],
     ];
