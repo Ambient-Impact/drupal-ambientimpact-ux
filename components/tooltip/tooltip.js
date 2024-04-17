3748e28d799a20c141609ee33555d41f02d613e5
Matei Stanca <i@ambientimpact.com>
2024-04-17 01:48:56 -0400

    Issue #1: Ported hide on out of bounds as a Tippy.js plug-in.

--- components/tooltip/tooltip.js
+++ components/tooltip/tooltip.js
@@ -141,13 +141,96 @@ AmbientImpact.addComponent('tooltip', function(aiTooltip, $) {
   //   }}
   // };
 
-  // this.hideOnOutOfBoundsPlugin = {
-  //   name: 'hideOnOutOfBounds',
-  //   defaultValue: true,
-  //   fn: function() { return {
-  //     // @todo
-  //   }}
-  // };
+  /**
+   * Hide on out of bounds Tippy.js plug-in.
+   *
+   * If enabled, this hides the tooltip if the reference element - i.e. the
+   * trigger - is scrolled out of view while the tooltip is open.
+   *
+   * @type {Object}
+   *
+   * @see https://popper.js.org/docs/v2/modifiers/hide/
+   *
+   * @todo Is there a Popper event or other internal thing we can listen to
+   *   instead of using a MutationObserver?
+   */
+  this.hideOnOutOfBoundsPlugin = {
+    name: 'hideOnOutOfBounds',
+    defaultValue: true,
+    fn: function() {
+
+      /**
+       * A MutationObserver to watch for the reference hidden attribute.
+       *
+       * @type {MutationObserver}
+       */
+      let observer;
+
+      /**
+       * The attribute name to watch for.
+       *
+       * Tippy.js/Popper adds this when the reference is out of view
+       * to .tippy-box
+       *
+       * @type {String}
+       */
+      const attributeName = 'data-reference-hidden';
+
+      function watch(instance) {
+
+        if (!instance.props.hideOnOutOfBounds) {
+          return;
+        }
+
+        /**
+         * The .tippy-box element wrapped in a jQuery collection.
+         *
+         * @type {jQuery}
+         */
+        const $tippyBox = $(instance.popper).find('.tippy-box');
+
+        observer = new MutationObserver(function(mutations) {
+
+          // If the attribute doesn't exist, it'll be undefined; if it does
+          // exist, it won't be undefined but an empty string.
+          if (typeof $tippyBox.attr(attributeName) !== 'undefined') {
+            instance.hide();
+          }
+
+        });
+
+        observer.observe($tippyBox[0], {
+          attributes:       true,
+          attributeFilter:  [attributeName],
+        });
+
+      };
+
+      function unwatch(instance) {
+
+        if (
+          !instance.props.hideOnOutOfBounds ||
+          typeof observer === 'undefined'
+        ) {
+          return;
+        }
+
+        observer.disconnect();
+
+      };
+
+      return {
+        onShow: watch,
+        onHide: unwatch,
+        // In the unlikely case the tooltip is still open on destroy, this
+        // ensures we clean up after ourselves by disconnecting the
+        // MutationObserver.
+        onDestroy: unwatch,
+      };
+
+    },
+
+  };
 
   /**
    * Title attribute Tippy.js plug-in.
@@ -257,6 +340,7 @@ AmbientImpact.addComponent('tooltip', function(aiTooltip, $) {
       plugins: [
         aiTooltip.debugPlugin,
         aiTooltip.hideOnEscPlugin,
+        aiTooltip.hideOnOutOfBoundsPlugin,
         aiTooltip.titleAttributePlugin,
       ],
       target: '[title]',
