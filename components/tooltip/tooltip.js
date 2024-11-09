5cc0a314e636114b78e81c44e82efab0a67b8501
Matei Stanca <i@ambientimpact.com>
2024-11-09 17:55:47 -0500

    Extended tooltip arrow bleed to 6 pixels.

--- components/tooltip/tooltip.js
+++ components/tooltip/tooltip.js
@@ -11,14 +11,14 @@ AmbientImpact.addComponent('tooltip', function(aiTooltip, $) {
   'use strict';
 
   /**
-   * Modified version of tippy.roundArrow to add a 2 pixel bleed at the bottom.
+   * Modified version of tippy.roundArrow to add a 6 pixel bleed at the bottom.
    *
    * This is intended to fix sub-pixel rendering issues where gaps can show up
    * due to the various transforms used.
    *
    * @type {String}
    */
-  const arrow = '<svg width="16" height="8" xmlns="http://www.w3.org/2000/svg"><path d="M8 .19c-1.07.005-2.15.878-3.33 2.328C1.796 6.034 0 6.048 0 6.048V8h16V6.047s-1.766.005-4.656-3.53C10.149 1.056 9.07.185 8 .19Z"/></svg>';
+  const arrow = '<svg width="16" height="12" xmlns="http://www.w3.org/2000/svg"><path d="M8 .19c-1.07.005-2.15.878-3.33 2.328C1.796 6.034 0 6.048 0 6.048V12h16V6.047s-1.766.005-4.656-3.53C10.149 1.056 9.07.185 8 .19Z"/></svg>';
 
   tippy.setDefaultProps({
     arrow: arrow,
